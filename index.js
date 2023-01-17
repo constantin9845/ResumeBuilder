2343c5b25d0488a9d8c87d0db6311840945be7a3
constantin9845 <constantin.y@outlook.com>
2023-01-17 00:47:40 -0800

    test

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ async function generatePdf(html) {
     return pdf;
 };
 
-console.log(puppeteer.executablePath())
+console.log(puppeteer.version)
 
 
 app.get('/result-display', async (req,res)=>{
